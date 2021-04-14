9a75de0377ad7dab3f7507436972d3bf86ed9411
YavariMain <58773408+YavariMain@users.noreply.github.com>
2021-04-14 17:18:28 +0430

    first commit

---
+++ pages/blog/_slu.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container-xl">
+    <div class="row">
+      <div class="col-xl-12">
+        <b-img
+          class="w-100"
+          src="https://picsum.photos/1024/400/?image=41"
+          fluid
+          alt="Responsive image"
+        ></b-img>
+      </div>
+    </div>
+    <div class="row pt-5">
+      <div class="col-xl-12">
+        <h3>title</h3>
+      </div>
+    </div>
+    <div class="row pt-3">
+      <div class="col-xl-12">
+        <p>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+    <div class="row pt-3">
+      <div class="col-xl-12">
+        <b-button href="#" variant="primary">add to cart</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        list:[],
+      }
+    },
+    mounted() {
+      this.getData()
+    },
+    methods:{
+      getData(){
+        this.$axios.get('/en/apiv2/catalog/get-product-list?slug=manufacturing-processing-machinery&page=1')
+          .then(resp=>{
+            this.list = resp.data.data.data;
+            console.log(this.list)
+          })
+      }
+    }
+  }
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="se-product">
+    <div class="container-xl">
+      <div class="row">
+        <div class="col-xl-12">
+          <h2>products cat title</h2>
+        </div>
+        <div class="col-xl-4" v-for="item in list">
+          <template>
+            <b-card
+              :title="item.location"
+              :img-src="item.image"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </b-card-text>
+              <div class="d-flex">
+                <b-button href="#" variant="primary">add to cart</b-button>
+                <NuxtLink
+                  :to="`/blog/${item.slug}`"
+                  style="margin-left: 5px"
+                >more</NuxtLink
+                >
+              </div>
+            </b-card>
+          </template>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list:[],
+    }
+  },
+  mounted() {
+    this.getData()
+  },
+  methods:{
+    getData(){
+      this.$axios.get('/en/apiv2/catalog/get-product-list?slug=manufacturing-processing-machinery&page=1')
+        .then(resp=>{
+          this.list = resp.data.data.data;
+          console.log(this.list)
+        })
+    }
+  }
+}
+</script>
